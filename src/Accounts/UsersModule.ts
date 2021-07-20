abe08805019fab857e3dd3cc834a3106ce86dd42
andreassef <andreassef02@gmail.com>
2021-07-19 22:27:17 -0400

    [NEW] - Novo modulo de usuario

---
+++ src/Accounts/UsersModule.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './usersController';
+import { UsersService } from './usersService';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
