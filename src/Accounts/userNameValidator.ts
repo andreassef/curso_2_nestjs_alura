be0dafc41275b8eadc7745cb1678f0d679bbb682
andreassef <andreassef02@gmail.com>
2021-07-19 22:28:24 -0400

    [NEW] - Valitador para nome único de usuário.

---
+++ src/Accounts/userNameValidator.ts
@@ -0,0 +1,33 @@
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { UsersService } from './usersService';
+
+@ValidatorConstraint()
+class IsUserAlreadyExistConstraint implements ValidatorConstraintInterface {
+  constructor(private usersService: UsersService) {}
+
+  validate(
+    name: string,
+    validationArguments?: ValidationArguments,
+  ): boolean | Promise<boolean> {
+    return !!!this.usersService.listOneByName(name);
+  }
+}
+
+export function IsUserAlreadyExist(validationOptions?: ValidationOptions) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsUserAlreadyExistConstraint,
+    });
+  };
+}
