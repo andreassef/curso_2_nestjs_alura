59d5c912846efba39e854c4e88dd0fe70de46e85
andreassef <andreassef02@gmail.com>
2021-07-19 22:27:02 -0400

    [NEW] - Servico de user

---
+++ src/Accounts/UsersService.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './User';
+
+@Injectable()
+export class UsersService {
+  private storage: User[] = [];
+  create(user: User): User {
+    this.storage.push(user);
+    return user;
+  }
+  list(): User[] {
+    return this.storage;
+  }
+
+  listOneByName(name: string): User {
+    const user = this.storage.find((index) => name === index.name);
+    return user;
+  }
+}
