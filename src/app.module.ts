d0b77670aed74aa68962334c09c24a6b05616b2d
andreassef <andreassef02@gmail.com>
2021-07-21 21:35:19 -0400

    [UPDATE] - Adicao de novo interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { UsersModule } from './Accounts/usersModule';
+import { HandleError } from './common/filter/HandleError';
+import { HandleResponseInterceptor } from './Core/Http/handle-response-interceptors';
 
 @Module({
   imports: [UsersModule],
   controllers: [],
   providers: [
     {
+      provide: APP_FILTER,
+      useClass: HandleError,
+    },
+    {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: HandleResponseInterceptor,
     },
   ],
 })
